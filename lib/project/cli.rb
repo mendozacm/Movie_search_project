7b2e01c786a95548a608a533e4120e62f106262a
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-12-04 01:33:05 +0000

    CLI controller

--- lib/project/cli.rb
+++ lib/project/cli.rb
@@ -1,12 +1,20 @@
 class CLI 
   
-  
   def self.testing_method
     puts "i work!"
   end
   
+end
+
+#CLI controller./
+class Project::CLI
+  def call
+    puts "what up"
+    list_deals
+  end
+  def list_deals
+    puts "today only blah blah blah"
+  end
   
   
 end
-
-puts "hello my  amigos"
