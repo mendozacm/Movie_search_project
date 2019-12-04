c6f70ea99019c8c69705576729735a19be80ca95
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-12-04 01:48:02 +0000

    up until goodbye

--- lib/project/cli.rb
+++ lib/project/cli.rb
@@ -8,13 +8,33 @@ end
 
 #CLI controller./
 class Project::CLI
+  
   def call
-    puts "what up"
     list_deals
+    menu
+    goodbye
   end
+  
   def list_deals
+    puts "what up"
     puts "today only blah blah blah"
   end
   
+  def menu
+    puts "enter the number on list you'd like to know more about or type 'exit':"
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1" 
+          puts "More info on deal 1..."
+      when "2"
+          puts "More info on deal 2"
+      end
+    end
+  end
   
+  def goodbye
+    puts "until next time"
+  end
 end
