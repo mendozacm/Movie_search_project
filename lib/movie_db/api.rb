fe302741e636a4d401790b8e5b5f2e96f7f97ee8
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-12-16 23:53:46 +0000

    fixed error where numbers in array would keep adding

--- lib/movie_db/api.rb
+++ lib/movie_db/api.rb
@@ -2,7 +2,7 @@ class MovieDB::API
   BASE_URL = 'http://api.themoviedb.org/3'
   API_KEY = 'cb09e4c3864336c419d217cc3e2f9387'
   
-  def self.query_movie_db(trending)
+  def self.query_movie_db
    results = RestClient.get("#{BASE_URL}/trending/movie/day?api_key=#{API_KEY}")
 
     json = JSON.parse(results)
--- lib/movie_db/cli.rb
+++ lib/movie_db/cli.rb
@@ -2,9 +2,11 @@ class MovieDB::CLI
   
   def start 
     puts "Welcome user"
+    MovieDB::API.query_movie_db
     menu
   end
   
+  def
   
   
   def menu
@@ -12,21 +14,15 @@ class MovieDB::CLI
     input = nil 
     while input != "exit"
       input = gets.strip
-      MovieDB::API.query_movie_db(input)
+      
         MovieDB::Trending.all.each.with_index(1) do |m, i|
           puts "#{i}. #{m. title}"
         end
       puts "What movie would you like to know more about? Just type in the number of the movie on the list! Or type 'exit' to exit"
       
       input = gets.strip
-      if input.to_i > 0 
-      movie = MovieDB::Trending.all[input.to_i - 1]
-      MovieDB::API.query_single_movie(movie)
-      
-      puts "Vote_average: #{movie.vote_average}"
-      puts "Vote_count: #{movie.vote_count}"
-      puts "Release_date: #{movie.release_date}"
-      puts "hit 'enter' to continue"
+      if input.to_i > 0 && input.to_i <= MovieDB::Trending.all.length
+        show_details
       elsif input == "exit"
       puts "Goodbye, have a great day!!"
       else
